020cf154130e49a16629035f5d2c48440d625071
GhostSP <141388145+Dahlabanteng@users.noreply.github.com>
2024-02-21 09:07:49 +0700

    Update twrp_Infinix-X678B.mk
    twrp

--- twrp_Infinix-X678B.mk
+++ twrp_Infinix-X678B.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X678B device
 $(call inherit-product, device/infinix/Infinix-X678B/device.mk)
 
 PRODUCT_DEVICE := Infinix-X678B
-PRODUCT_NAME := omn_Infinix-X678B
+PRODUCT_NAME := twrp_Infinix-X678B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X678B
 PRODUCT_MANUFACTURER := infinix
