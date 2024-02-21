20761a7b6e96cdc4e7e403aee90aabe4e0eb3e04
GhostSP <141388145+Dahlabanteng@users.noreply.github.com>
2024-02-21 12:29:51 +0900

    Update twrp_X678B.mk

--- twrp_X678B.mk
+++ twrp_X678B.mk
@@ -5,16 +5,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+#  from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from our custom product configuration.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X678B device
-$(call inherit-product, device/infinix/Infinix-X678B/device.mk)
-
+# Device specific configs.
+$(call inherit-product, device/infinix/X678/device.mk)
 PRODUCT_DEVICE := X678B
 PRODUCT_NAME := twrp_X678B
 PRODUCT_BRAND := Infinix
