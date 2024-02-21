3d94f296172979bf075c41b2a7771c976e60e1cc
GhostSP <141388145+Dahlabanteng@users.noreply.github.com>
2024-02-21 12:35:24 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X678B
+LOCAL_PATH := device/infinix/X678B
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
